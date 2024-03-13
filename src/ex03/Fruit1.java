ad30b29117ab01ce86245e6d100a2a58830e9b08
anashkinasvetlana <anashkinasviatlana@gmail.com>
2024-03-13 17:34:46 +0300

    Добавляем печать

--- src/ex03/Fruit1.java
+++ src/ex03/Fruit1.java
@@ -11,7 +11,7 @@ public class Fruit1 {
         fruits[1] = new Fruit1("Apple");
         fruits[2] = new Fruit1("Avocado");
         fruits[3] = new Fruit1("Apricot");
-        System.out.printf(fruits);
+        System.out.printf("Фрукты" + fruits);
 
     }
 }
