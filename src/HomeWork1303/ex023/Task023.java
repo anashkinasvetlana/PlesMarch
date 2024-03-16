694d6b2b6bc4d486d7b3330d1eb81ac8550bea55
anashkinasvetlana <anashkinasviatlana@gmail.com>
2024-03-16 12:10:05 +0300

    Решено

---
+++ src/HomeWork1303/ex023/Task023.java
@@ -0,0 +1,15 @@
+package HomeWork1303.ex023;
+
+public class Task023 {
+    public static void main(String[] args) {
+
+        String[] num = {"dog","cat","bird",null,"pig","deer"};
+        int x = num.length;
+        for (int i=1;i<x;i=i+2) {
+            if (num [i]==null) {
+                break;
+            }
+            System.out.println(" i = " +num[i]);
+        }
+        }
+    }
